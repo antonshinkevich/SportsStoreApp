267b18df67dcf29993ae10abbe71a7f6f12f98d0
antonshinkevich <antons.hinkevich@gmail.com>
2022-07-27 22:33:27 +0300

    Configure the project. Create first Model, Controller and View

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
