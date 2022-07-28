69c7845b14ed2bdba5c7dfead8f32b15f244baa5
antonshinkevich <antons.hinkevich@gmail.com>
2022-07-28 11:12:02 +0300

    Create product repository and DI for it

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Models;
+
+class EFStoreRepository : IStoreRepository
+{
+    private readonly StoreDbContext _context;
+
+    public EFStoreRepository(StoreDbContext ctx)
+    {
+        _context = ctx;
+    }
+
+    public IQueryable<Product> Products => _context.Products;
+}
