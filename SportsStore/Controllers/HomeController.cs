5286038f53708ded1480ab04eb5f377d2058c0f4
antonshinkevich <antons.hinkevich@gmail.com>
2022-07-29 08:51:16 +0300

    Add TagHelper and ViewModels for pagination. Add some tests as well

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly IStoreRepository _repository;
+    public int PageSize = 4;
+
+    public HomeController(IStoreRepository repository)
     {
-        return View();
+        _repository = repository;
+    }
+
+    public ViewResult Index(int productPage = 1)
+    {
+        return View(new ProductsListViewModel
+        {
+            Products = _repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = _repository.Products.Count()
+            }
+        });
     }
 }
