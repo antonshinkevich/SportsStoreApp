0273ba81fab737d3859661d6b3a7189266c4c328
antonshinkevich <antons.hinkevich@gmail.com>
2022-09-26 11:42:26 +0300

    Add Razor page for Cart and add font-awesome library

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -17,7 +17,8 @@ namespace SportsStore.Pages
         {
             ReturnUrl = returnUrl ?? "/";
             // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
-        } 
+        }
+
         public IActionResult OnPost(long productId, string returnUrl)
         {
             Product? product = repository.Products
@@ -27,6 +28,13 @@ namespace SportsStore.Pages
                 Cart.AddItem(product, 1);
             }
             return RedirectToPage(new { returnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => 
+                cl.Product.ProductID == productId).Product);
+            return RedirectToPage(new { ReturnUrl = returnUrl });
         }
     }
 }
