21bb6c798c15e4f210737a84cb251fa6529096ac
antonshinkevich <antons.hinkevich@gmail.com>
2022-10-03 13:58:31 +0300

    Add Order model and dbContext for it. Add page for successful order

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repository, Cart cart)
+        {
+            this.repository = repository;
+            this.cart = cart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View(new Order());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                cart.Clear();
+                return RedirectToPage("/Completed", new { orderId = order.OrderID });
+            }
+
+            return View(order);
+        }
+    }
+}
